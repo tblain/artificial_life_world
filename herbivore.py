a2237f7d9a0a340102d954a6395aca1f095567de
tblain <tblain69@gmail.com>
2019-09-02 22:20:26 +0200

    mecanismes de regulation de la pop d'herbivore

--- herbivore.py
+++ herbivore.py
@@ -1,6 +1,7 @@
 from bot import Bot
 import numpy as np
 
+
 class Herbivore(Bot):
     def __init__(self, map, x, y, sim):
         Bot.__init__(self, map, x, y, sim)
@@ -14,7 +15,7 @@ class Herbivore(Bot):
 
         actions = [0, 0, 0, 0, 0, 0, 0, 0]
 
-        if self.g_energy() > 300:
+        if self.g_energy() > 300 and self.can_reproduce(taille_limite=True):
             actions[6] = 1000
             pass
 
@@ -36,12 +37,17 @@ class Herbivore(Bot):
         # print(actions)
         return np.argmax(actions)
 
+    def can_reproduce(self, taille_limite=False):
+        return self.g_cd_repro() == 0 and (
+            not taille_limite or len(self.sim.bots) < 750
+        )
+
     def mitose(self):
         if self.g_cd_repro() == 0:
 
-            self.incr_cd_repro(50) # the bot can't reproduce for 50 tick
-            self.incr_energy(-5) # loose of energy to make the child
-            energy_to_child = 5 # energy that will be transfered to the child
+            self.incr_cd_repro(50)  # the bot can't reproduce for 50 tick
+            self.incr_energy(-5)  # loose of energy to make the child
+            energy_to_child = 5  # energy that will be transfered to the child
 
             x = -1
             y = -1
@@ -67,7 +73,7 @@ class Herbivore(Bot):
                 if self.map.board[self.x + 1, self.y, 0] == 0:
                     x = self.x + 1
                     y = self.y
-            
+
             else:
                 # no place to put the child
                 self.incr_energy(energy_to_child)
@@ -75,7 +81,8 @@ class Herbivore(Bot):
 
             if x == -1:
                 pass
-            else:    
+            else:
                 new_bot = Herbivore(self.map, self.x + 1, self.y, self.sim)
                 new_bot.s_energy(energy_to_child)
                 self.sim.add_bots([new_bot])
+
