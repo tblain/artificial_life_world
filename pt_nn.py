672fd88fa11bbeb797f1cfeddbecc9246b7b3176
tblain <tblain69@gmail.com>
2019-12-19 21:26:32 +0100

    debut de NN pytorch

---
+++ pt_nn.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = torch.nn.Linear(10, 10).to('cuda:0')
+        self.relu = torch.nn.ReLU()
+        self.net2 = torch.nn.Linear(10, 5).to('cuda:1')
+
+    def forward(self, x):
+        x = self.relu(self.net1(x.to('cuda:0')))
+        return self.net2(x.to('cuda:1'))
